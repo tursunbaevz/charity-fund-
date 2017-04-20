ebd0097de66ba24c112dfe3fee47c6d2aa67bf8b
Eric Neifert <eric.neifert@gmail.com>
2017-04-20 15:43:03 +0600

    Can now see images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :thumb do
     process :resize_to_limit => [200, 200]
-    end
+  end
+
+  version :med do
+    process :resize_to_limit => [500, 500]
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
